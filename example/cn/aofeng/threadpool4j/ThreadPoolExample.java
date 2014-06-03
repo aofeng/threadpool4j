b830632c83d2c6a30f8ff7e566e9a28eed133710
aofeng <aofengblog@163.com>
2014-06-03 11:44:49 +0800

    增加耗时的操作，方便观察线程的状态和线程池队列的任务剩余量

--- example/cn/aofeng/threadpool4j/ThreadPoolExample.java
+++ example/cn/aofeng/threadpool4j/ThreadPoolExample.java
@@ -1,5 +1,10 @@
 package cn.aofeng.threadpool4j;
 
+import java.util.Arrays;
+import java.util.Random;
+
+import cn.aofeng.common4j.ILifeCycle;
+
 /**
  * 多线程池调用代码示例。
  * 
@@ -7,19 +12,39 @@ package cn.aofeng.threadpool4j;
  */
 public class ThreadPoolExample {
 
+    // 耗时的操作（配置低一些的机器小心CPU 100％，反应慢）
+    public void needSomeTime() {
+        int len = 100000;
+        String[] intArray = new String[len];
+        Random random = new Random(len);
+        
+        // 初始化数组
+        for (int i = 0; i < len; i++) {
+            intArray[i] = String.valueOf(random.nextInt());
+        }
+        
+        // 排序
+        Arrays.sort(intArray);
+    }
+    
     /**
      * @param args
      * @throws Exception 
      */
     public static void main(String[] args) throws Exception {
-        ThreadPoolImpl threadPool = ThreadPoolImpl.getInstance();
-        threadPool.init();
+        ILifeCycle th = ThreadPoolImpl.getInstance();
+        th.init();
+        ThreadPool threadPool = ThreadPoolImpl.getInstance();
+        final ThreadPoolExample self = new ThreadPoolExample();
         
         for (int i = 0; i < 1000000; i++) {
             Runnable task1 = new Runnable() {
                 @Override
                 public void run() {
-                    System.out.println("执行异步任务1");
+                    long startTime = System.currentTimeMillis();
+                    self.needSomeTime();
+                    long endTime = System.currentTimeMillis();
+                    System.out.println("执行异步任务1，耗时："+(endTime-startTime));
                 }
             };
             threadPool.submit(task1);
@@ -27,7 +52,10 @@ public class ThreadPoolExample {
             Runnable task2 = new Runnable() {
                 @Override
                 public void run() {
-                    System.out.println("执行异步任务2");
+                    long startTime = System.currentTimeMillis();
+                    self.needSomeTime();
+                    long endTime = System.currentTimeMillis();
+                    System.out.println("执行异步任务2，耗时："+(endTime-startTime));
                 }
             };
             threadPool.submit(task2, "other");
@@ -35,7 +63,7 @@ public class ThreadPoolExample {
             Thread.sleep(50);
         }
         
-        threadPool.destroy();
+        th.destroy();
     }
 
 }
