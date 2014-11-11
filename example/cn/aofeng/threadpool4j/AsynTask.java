7df443e162a4438210e5363ee8ac1f845696bbdd
NieYong <aofengblog@163.com>
2014-11-11 10:44:00 +0800

    重构：将异步任务的代码抽取成单独的类

---
+++ example/cn/aofeng/threadpool4j/AsynTask.java
@@ -0,0 +1,31 @@
+package cn.aofeng.threadpool4j;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class AsynTask implements Runnable {
+
+    // 耗时的操作（配置低一些的机器小心CPU 100％，反应慢）
+    public void needSomeTime() {
+        int len = 50000;
+        String[] intArray = new String[len];
+        Random random = new Random(len);
+        
+        // 初始化数组
+        for (int i = 0; i < len; i++) {
+            intArray[i] = String.valueOf(random.nextInt());
+        }
+        
+        // 排序
+        Arrays.sort(intArray);
+    }
+    
+    @Override
+    public void run() {
+        long startTime = System.currentTimeMillis();
+        needSomeTime();
+        long endTime = System.currentTimeMillis();
+        System.out.println("执行任务耗时："+(endTime-startTime));
+    }
+
+}
--- example/cn/aofeng/threadpool4j/ThreadPoolExample.java
+++ example/cn/aofeng/threadpool4j/ThreadPoolExample.java
@@ -1,8 +1,5 @@
 package cn.aofeng.threadpool4j;
 
-import java.util.Arrays;
-import java.util.Random;
-
 import cn.aofeng.common4j.ILifeCycle;
 
 /**
@@ -11,21 +8,6 @@ import cn.aofeng.common4j.ILifeCycle;
  * @author <a href="mailto:aofengblog@163.com">聂勇</a>
  */
 public class ThreadPoolExample {
-
-    // 耗时的操作（配置低一些的机器小心CPU 100％，反应慢）
-    public void needSomeTime() {
-        int len = 100000;
-        String[] intArray = new String[len];
-        Random random = new Random(len);
-        
-        // 初始化数组
-        for (int i = 0; i < len; i++) {
-            intArray[i] = String.valueOf(random.nextInt());
-        }
-        
-        // 排序
-        Arrays.sort(intArray);
-    }
     
     /**
      * @param args
@@ -35,31 +17,11 @@ public class ThreadPoolExample {
         ThreadPoolImpl temp = new ThreadPoolImpl();
         ILifeCycle th = temp;
         th.init();
-        ThreadPool threadPool = temp;
-        final ThreadPoolExample self = new ThreadPoolExample();
         
-        for (int i = 0; i < 1000000; i++) {
-            Runnable task1 = new Runnable() {
-                @Override
-                public void run() {
-                    long startTime = System.currentTimeMillis();
-                    self.needSomeTime();
-                    long endTime = System.currentTimeMillis();
-                    System.out.println("执行异步任务1，耗时："+(endTime-startTime));
-                }
-            };
-            threadPool.submit(task1);
-            
-            Runnable task2 = new Runnable() {
-                @Override
-                public void run() {
-                    long startTime = System.currentTimeMillis();
-                    self.needSomeTime();
-                    long endTime = System.currentTimeMillis();
-                    System.out.println("执行异步任务2，耗时："+(endTime-startTime));
-                }
-            };
-            threadPool.submit(task2, "other");
+        ThreadPool threadPool = temp;
+        for (int i = 0; i < 10000; i++) {
+            threadPool.submit(new AsynTask());
+            threadPool.submit(new AsynTask(), "other");
             
             Thread.sleep(50);
         }
