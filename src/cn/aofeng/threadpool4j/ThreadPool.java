28f4f2189a4c5244ea0aa7779fd81bf66f191987
NieYong <aofengblog@163.com>
2014-11-11 10:46:52 +0800

    增加三个功能特性： 1、支持执行单个实现Callable接口的异步任务。 2、支持查询指定名称的线程池是否存在。 3、支持执行多个实现Callable接口的异步任务，并可以设置超时时间。

--- src/cn/aofeng/threadpool4j/ThreadPool.java
+++ src/cn/aofeng/threadpool4j/ThreadPool.java
@@ -1,7 +1,12 @@
 package cn.aofeng.threadpool4j;
 
 import java.lang.management.ThreadInfo;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CancellationException;
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 线程池。
@@ -11,25 +16,101 @@ import java.util.concurrent.Future;
 public interface ThreadPool {
 
     /**
-     * 提交一个异步任务给默认的线程池执行。
+     * 提交一个不需要返回值的异步任务给默认的线程池执行。
      * 
      * @param task 实现了{@link Runnable}接口的异步任务
      * @return 异步任务执行的结果
-     * @throws IllegalArgumentException 如果指定的<code>task</code>为null
+     * @throws IllegalArgumentException 指定的任务（<code>task</code>）为null
      * @see #submit(Runnable, String)
      */
     public Future<?> submit(Runnable task);
     
     /**
-     * 提交一个异步任务给指定的线程池执行。
+     * 提交一个不需要返回值的异步任务给指定的线程池执行。
      * 
      * @param task 实现了{@link Runnable}接口的异步任务
      * @param threadpoolName 线程池名称
      * @return 异步任务执行的结果
-     * @throws IllegalArgumentException 如果指定的<code>task</code>为null，或者指定的线程池名称不存在
+     * @throws IllegalArgumentException 出现以下情况时抛出：
+     * <ul>
+     *     <li>指定的任务（<code>task</code>）为null；</li>
+     *     <li>指定的线程池名称（<code>threadpoolName</code>）为null，""或全是空白字符；</li>
+     *     <li>指定的线程池不存在。</li>
+     * </ul>
      */
     public Future<?> submit(Runnable task, String threadpoolName);
+    
+    /**
+     * 提交一个需要返回值的异步任务给默认的线程池执行。
+     * 
+     * @param task 实现了{@link Callable}接口的异步任务
+     * @return 异步任务执行的结果
+     * @throws IllegalArgumentException 指定的任务（<code>task</code>）为null
+     * @see #submit(Callable, String)
+     */
+    public <T> Future<T> submit(Callable<T> task);
+    
+    /**
+     * 提交一个需要返回值的异步任务给指定的线程池执行。
+     * 
+     * @param task 实现了{@link Callable}接口的异步任务
+     * @param threadpoolName 线程池名称
+     * @return 异步任务执行的结果
+     * @throws IllegalArgumentException 出现以下情况时抛出：
+     * <ul>
+     *     <li>指定的任务（<code>task</code>）为null；</li>
+     *     <li>指定的线程池名称（<code>threadpoolName</code>）为null，""或全是空白字符；</li>
+     *     <li>指定的线程池不存在。</li>
+     * </ul>
+     */
+    public <T> Future<T> submit(Callable<T> task, String threadpoolName);
+    
+    /**
+     * 在线程池"default"中执行多个需要返回值的异步任务，并设置超时时间。
+     * 
+     * @param tasks 实现了{@link Runnable}接口的异步任务列表
+     * @param timeout 任务执行超时时间
+     * @param timeoutUnit 超时时间的单位
+     * @return {@link Future}列表。注：如果在指定的时间内，有任务没有执行完，在执行Future.get操作时将抛出{@link CancellationException}。
+     * @throws IllegalArgumentException 出现以下情况时抛出：
+     * <ul>
+     *     <li>指定的任务列表（<code>tasks</code>）为null或是空列表；</li>
+     *     <li>指定的线程池不存在。</li>
+     *     <li>指定的超时时间（<code>timeout</code>）小于或等于0</li>
+     * </ul>
+     * @see #invokeAll(List, String, int, TimeUnit)
+     */
+    public <T> List<Future<T>> invokeAll(Collection<Callable<T>> tasks, 
+            long timeout, TimeUnit timeoutUnit);
+    
+    /**
+     * 在指定的线程池中执行多个需要返回值的异步任务，并设置超时时间。
+     * 
+     * @param tasks 实现了{@link Runnable}接口的异步任务列表
+     * @param timeout 任务执行超时时间
+     * @param timeoutUnit 超时时间的单位
+     * @param threadpoolName 线程池名称
+     * @return {@link Future}列表。注：如果在指定的时间内，有任务没有执行完，在执行Future.get操作时将抛出{@link CancellationException}。
+     * @throws IllegalArgumentException 出现以下情况时抛出：
+     * <ul>
+     *     <li>指定的任务列表（<code>tasks</code>）为null或是空列表；</li>
+     *     <li>指定的线程池名称（<code>threadpoolName</code>）为null，""或全是空白字符；</li>
+     *     <li>指定的线程池不存在。</li>
+     *     <li>指定的超时时间（<code>timeout</code>）小于或等于0</li>
+     * </ul>
+     */
+    public <T> List<Future<T>> invokeAll(Collection<Callable<T>> tasks,  
+            long timeout, TimeUnit timeoutUnit, String threadpoolName);
 
+    /**
+     * 查询指定名称的线程池是否存在。
+     * 
+     * @param threadpoolName 线程池名称
+     * @return 如果指定的线程池存在，返回true；否则，返回true。
+     * @throws IllegalArgumentException 指定的线程池名称（<code>threadpoolName</code>）为null，""或全是空白字符
+     */
+    public boolean isExists(String threadpoolName);
+    
     /**
      * 获取线程池的信息。如：线程池容量，队列容量。
      * 
--- src/cn/aofeng/threadpool4j/ThreadPoolImpl.java
+++ src/cn/aofeng/threadpool4j/ThreadPoolImpl.java
@@ -2,10 +2,12 @@ package cn.aofeng.threadpool4j;
 
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
 import java.util.concurrent.ThreadPoolExecutor;
@@ -14,6 +16,7 @@ import java.util.concurrent.TimeUnit;
 import org.apache.log4j.Logger;
 
 import cn.aofeng.common4j.ILifeCycle;
+import cn.aofeng.common4j.lang.StringUtil;
 import cn.aofeng.threadpool4j.job.ThreadPoolStateJob;
 import cn.aofeng.threadpool4j.job.ThreadStateJob;
 
@@ -23,6 +26,9 @@ import cn.aofeng.threadpool4j.job.ThreadStateJob;
  * @author <a href="mailto:aofengblog@163.com">聂勇</a>
  */
 public class ThreadPoolImpl implements ILifeCycle, ThreadPool {
+
+    /** 默认的线程池名称 */
+    private static final String DEFAULT_THREAD_POOL = "default";
 
     private static Logger _logger = Logger.getLogger(ThreadPoolImpl.class);    
     
@@ -92,23 +98,95 @@ public class ThreadPoolImpl implements ILifeCycle, ThreadPool {
     }
     
     public Future<?> submit(Runnable task) {
-        return submit(task, "default");
+        return submit(task, DEFAULT_THREAD_POOL);
     }
     
     public Future<?> submit(Runnable task, String threadpoolName) {
-        ExecutorService threadPool = _multiThreadPool.get(threadpoolName);
         if (null == task) {
             throw new IllegalArgumentException("task is null");
         }
-        if (null == threadPool) {
-            throw new IllegalArgumentException( String.format("thread pool %s not exists", threadpoolName) );
-        }
         
+        ExecutorService threadPool = getExistsThreadPool(threadpoolName);
         if (_logger.isDebugEnabled()) {
             _logger.debug("submit a task to thread pool "+threadpoolName);
         }
         
         return threadPool.submit(task);
+    }
+
+    private ExecutorService getThreadPool(String threadpoolName) {
+        if (StringUtil.isBlank(threadpoolName)) {
+            throw new IllegalArgumentException("thread pool name is empty");
+        }
+        
+        ExecutorService threadPool = _multiThreadPool.get(threadpoolName);
+        
+        return threadPool;
+    }
+    
+    private ExecutorService getExistsThreadPool(String threadpoolName) {
+        ExecutorService threadPool = getThreadPool(threadpoolName);
+        if (null == threadPool) {
+            throw new IllegalArgumentException( String.format("thread pool %s not exists", threadpoolName) );
+        }
+        
+        return threadPool;
+    }
+    
+    @Override
+    public <T> Future<T> submit(Callable<T> task) {
+        return submit(task, DEFAULT_THREAD_POOL);
+    }
+    
+    @Override
+    public <T> Future<T> submit(Callable<T> task, String threadpoolName) {
+        if (null == task) {
+            throw new IllegalArgumentException("task is null");
+        }
+        
+        ExecutorService threadPool = getExistsThreadPool(threadpoolName);
+        if (_logger.isDebugEnabled()) {
+            _logger.debug("submit a task to thread pool "+threadpoolName);
+        }
+        
+        return threadPool.submit(task);
+    }
+    
+    @Override
+    public <T> List<Future<T>> invokeAll(Collection<Callable<T>> tasks, 
+            long timeout, TimeUnit timeoutUnit) {
+        return invokeAll(tasks, timeout, timeoutUnit, DEFAULT_THREAD_POOL);
+    }
+    
+    @Override
+    public <T> List<Future<T>> invokeAll(Collection<Callable<T>> tasks,
+            long timeout, TimeUnit timeoutUnit, String threadpoolName) {
+        if (null == tasks || tasks.isEmpty()) {
+            throw new IllegalArgumentException("task list is null or empty");
+        }
+        if (timeout <= 0) {
+            throw new IllegalArgumentException("timeout less than or equals zero");
+        }
+        
+        ExecutorService threadPool = getExistsThreadPool(threadpoolName);
+        if (_logger.isDebugEnabled()) {
+            _logger.debug("invoke task list in thread pool "+threadpoolName);
+        }
+        
+        try {
+            return threadPool.invokeAll(tasks, timeout, timeoutUnit);
+        } catch (InterruptedException e) {
+            _logger.error("invoke task list occurs error", e);
+        }
+        
+        return null;
+    }
+    
+    @Override
+    public boolean isExists(String threadpoolName) {
+        ExecutorService threadPool = getThreadPool(threadpoolName);
+        
+        return (null == threadPool ? false : true);
     }
     
     @Override
